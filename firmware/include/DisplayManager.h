234560669feaeabdc6d43ea5a54ac624c1f769fe
atoomnetmarc <atoomnetmarc@mail1.atoomnet.net>
2025-06-28 11:21:19 +0200

    Render digit pattern more elegantly.

--- firmware/include/DisplayManager.h
+++ firmware/include/DisplayManager.h
@@ -16,26 +16,14 @@ public:
     void initialize();
     void displayNumber(int16_t n, bool open = true);
     void showPattern(uint8_t digit, uint8_t pattern);
+    uint8_t getPattern(char character);
     void showText(const char *text);
 
 private:
     LedControl lc;
 
-    // Display patterns
-    static const uint8_t CHAR_A = 0b01110111;
-    static const uint8_t CHAR_BLANK = 0b00000000;
-    static const uint8_t CHAR_EQUALS = 0b00001001;
-    static const uint8_t CHAR_F = 0b01000111;
-    static const uint8_t CHAR_H = 0b00110111;
-    static const uint8_t CHAR_I = 0b00010000;
-    static const uint8_t CHAR_N = 0b01110110;
-    static const uint8_t CHAR_N_SMALL = 0b00010101;
-    static const uint8_t CHAR_P = 0b01100111;
-    static const uint8_t CHAR_Q = 0b01110011;
-    static const uint8_t CHAR_T = 0b00001111;
-    static const uint8_t CHAR_V = 0b00011100;
-    static const uint8_t MINUS_SIGN = 0b00000001;
-    static const uint8_t UNDERSCORE = 0b00001000;
+    static const uint8_t PATTERN_UNDEFINED = 0b11001001;
+    static const uint8_t patterns[96];
 };
 
 #endif
--- firmware/src/DisplayManager.cpp
+++ firmware/src/DisplayManager.cpp
@@ -21,31 +21,25 @@ void DisplayManager::initialize() {
 
 void DisplayManager::displayNumber(int16_t n, bool open) {
     if (n < 0) {
-        showPattern(0, 0);
-        showPattern(1, MINUS_SIGN);
-        showPattern(2, MINUS_SIGN);
-        showPattern(3, 0);
+        DisplayManager::showText(" -- ");
         return;
     }
 
     if (n > 9999) {
-        showPattern(0, 0);
-        showPattern(1, DisplayManager::CHAR_H);
-        showPattern(2, DisplayManager::CHAR_H);
-        showPattern(3, 0);
+        DisplayManager::showText(" HH ");
         return;
     }
 
     if (n > 999) {
         lc.setChar(0, 0, n / 1000, false);
     } else {
-        showPattern(0, CHAR_N_SMALL);
+        showPattern(0, DisplayManager::getPattern('n'));
     }
 
     if (n > 99) {
         lc.setChar(0, 1, (n % 1000) / 100, false);
     } else {
-        showPattern(1, CHAR_EQUALS);
+        showPattern(1, DisplayManager::getPattern('='));
     }
 
     if (n > 9) {
@@ -64,52 +58,112 @@ void DisplayManager::showPattern(uint8_t digit, uint8_t pattern) {
 void DisplayManager::showText(const char *text) {
     // Display up to 4 characters
     for (uint8_t i = 0; i < 4 && text[i] != '\0'; i++) {
-        switch (text[i]) {
-            case ' ':
-                showPattern(i, CHAR_BLANK);
-                break;
-            case '-':
-                showPattern(i, MINUS_SIGN);
-                break;
-            case '=':
-                showPattern(i, CHAR_EQUALS);
-                break;
-            case 'A':
-                showPattern(i, CHAR_A);
-                break;
-            case 'F':
-                showPattern(i, CHAR_F);
-                break;
-            case 'i':
-                showPattern(i, CHAR_I);
-                break;
-            case 'M':
-                showPattern(i, CHAR_N);
-                break;
-            case 'n':
-                showPattern(i, CHAR_N_SMALL);
-                break;
-            case 'N':
-                showPattern(i, CHAR_N);
-                break;
-            case 'P':
-                showPattern(i, CHAR_P);
-                break;
-            case 'Q':
-                showPattern(i, CHAR_Q);
-                break;
-            case 't':
-                showPattern(i, CHAR_T);
-                break;
-            case 'v':
-                showPattern(i, CHAR_V);
-                break;
-            case '_':
-                showPattern(i, UNDERSCORE);
-                break;
-            default:
-                showPattern(i, 0);
-                break;
-        }
+        showPattern(i, DisplayManager::getPattern(text[i]));
     }
 }
+
+uint8_t DisplayManager::getPattern(char character) {
+    return DisplayManager::patterns[(int)character - 32];
+}
+
+/**
+ * 7-segment led to bit mapping: dp, a, b, c, d, e, f, g.
+ */
+const uint8_t DisplayManager::patterns[96] = {
+    0b00000000,        // space
+    PATTERN_UNDEFINED, // !
+    PATTERN_UNDEFINED, // "
+    PATTERN_UNDEFINED, // #
+    PATTERN_UNDEFINED, // $
+    PATTERN_UNDEFINED, // %
+    PATTERN_UNDEFINED, // &
+    PATTERN_UNDEFINED, // '
+    PATTERN_UNDEFINED, // (
+    PATTERN_UNDEFINED, // )
+    PATTERN_UNDEFINED, // *
+    PATTERN_UNDEFINED, // +
+    PATTERN_UNDEFINED, // ,
+    0b00000001,        // -
+    PATTERN_UNDEFINED, // .
+    PATTERN_UNDEFINED, // /
+    0b01111110,        // 0
+    0b00110000,        // 1
+    0b01101101,        // 2
+    0b01111001,        // 3
+    0b00110011,        // 4
+    0b01011011,        // 5
+    0b01011111,        // 6
+    0b01110000,        // 7
+    0b01111111,        // 8
+    0b01111011,        // 9
+    PATTERN_UNDEFINED, // :
+    PATTERN_UNDEFINED, // ;
+    PATTERN_UNDEFINED, // <
+    0b00001001,        // =
+    PATTERN_UNDEFINED, // >
+    PATTERN_UNDEFINED, // ?
+    PATTERN_UNDEFINED, // @
+    0b01110111,        // A
+    PATTERN_UNDEFINED, // B
+    PATTERN_UNDEFINED, // C
+    PATTERN_UNDEFINED, // D
+    PATTERN_UNDEFINED, // E
+    0b01000111,        // F
+    PATTERN_UNDEFINED, // G
+    0b00110111,        // H
+    0b00010000,        // I
+    PATTERN_UNDEFINED, // J
+    PATTERN_UNDEFINED, // K
+    PATTERN_UNDEFINED, // L
+    0b01110110,        // N
+    PATTERN_UNDEFINED, // M
+    PATTERN_UNDEFINED, // O
+    0b01100111,        // P
+    0b01110011,        // Q
+    PATTERN_UNDEFINED, // R
+    0b00001111,        // T
+    PATTERN_UNDEFINED, // S
+    PATTERN_UNDEFINED, // U
+    0b00011100,        // V
+    0b00111111,        // W
+    PATTERN_UNDEFINED, // X
+    PATTERN_UNDEFINED, // Y
+    PATTERN_UNDEFINED, // Z
+    PATTERN_UNDEFINED, // [
+    PATTERN_UNDEFINED, // backslash
+    PATTERN_UNDEFINED, // ]
+    PATTERN_UNDEFINED, // ^
+    0b00001000,        // _
+    0b00000010,        // `
+    0b01110111,        // a
+    PATTERN_UNDEFINED, // b
+    PATTERN_UNDEFINED, // c
+    PATTERN_UNDEFINED, // d
+    PATTERN_UNDEFINED, // e
+    PATTERN_UNDEFINED, // f
+    PATTERN_UNDEFINED, // g
+    PATTERN_UNDEFINED, // h
+    PATTERN_UNDEFINED, // i
+    PATTERN_UNDEFINED, // j
+    PATTERN_UNDEFINED, // k
+    PATTERN_UNDEFINED, // l
+    PATTERN_UNDEFINED, // m
+    0b00010101,        // n
+    PATTERN_UNDEFINED, // o
+    PATTERN_UNDEFINED, // p
+    PATTERN_UNDEFINED, // q
+    PATTERN_UNDEFINED, // r
+    PATTERN_UNDEFINED, // s
+    0b00001111,        // t
+    PATTERN_UNDEFINED, // u
+    PATTERN_UNDEFINED, // v
+    0b00111111,        // w
+    PATTERN_UNDEFINED, // x
+    PATTERN_UNDEFINED, // y
+    PATTERN_UNDEFINED, // z
+    PATTERN_UNDEFINED, // {
+    0b00000110,        // |
+    PATTERN_UNDEFINED, // }
+    0b01000000,        // ~
+    0b00000000,        // (del)
+};
